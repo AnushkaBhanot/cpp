335ea3ccdef904cf06f84a5b2be5ec1201c18ac7
Anushka Bhanot <83168678+AnushkaBhanot@users.noreply.github.com>
2022-08-08 14:51:53 +0530

    Create factorSum.cpp
    Consider the number 18.
    
    Sum of factors = 1 + 2 + 3 + 6 + 9 + 18
    
    Writing divisors as powers of prime factors.
    Sum of factors = (2^0)(3^0) + (2^1)(3^0) + (2^0)(3^1) + (2^1)(3^1) + (2^0)(3^2) + (2^1)(3^2)
                   = (2^0)(3^0) + (2^0)(3^1) + (2^0)(3^2) + (2^1)(3^0) + (2^1)(3^1) + (2^1)(3^2)
                   = (2^0)(3^0 + 3^1 + 3^2) + (2^1)(3^0 + 3^1 + 3^2)
                   = (2^0 + 2^1)(3^0 + 3^1 + 3^2)
    If we take a closer look, we can notice that the
    above expression is in the form.
    (1 + p1) * (1 + p2 + p2^2)
    Where p1 = 2 and p2 = 3 and 18 = 2^1*3^2

---
+++ factorSum.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sumOfFactors(int n)
+{
+    int res=1;
+    for(int i=2; i<=sqrt(n); i++)
+    {
+        int curr_sum=1;
+        int curr_term=1;
+        while(n%i == 0)
+        {
+            n=n/i;
+            curr_term*=i;
+            curr_sum+= curr_term;
+        }
+        res*=curr_sum;
+    }
+    if(n>=2)
+     res*=(1+n);
+    return res;
+}
+
+int main()
+{
+    int n=30;
+    cout<<sumOfFactors(n);
+    return 0;
+}
