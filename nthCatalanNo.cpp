45af7ff8b84c44d9cb43644419313642d5d30986
Anushka Bhanot <83168678+AnushkaBhanot@users.noreply.github.com>
2022-08-12 11:57:33 +0530

    Create nthCatalanNo.cpp

---
+++ nthCatalanNo.cpp
@@ -0,0 +1,39 @@
+// The first few Catalan numbers for n = 0, 1, 2, 3, … are 1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, …
+
+#include<bits/stdc++.h>
+using namespace std;
+
+unsigned long int binomialCoeff(unsigned int n, unsigned int k)
+{
+    unsigned long int res = 1;
+
+    // Since C(n, k) = C(n, n-k)
+    if (k > n - k)
+        k = n - k;
+
+    // Calculate value of [n*(n-1)*---*(n-k+1)] /[k*(k-1)*---*1]
+    for (int i = 0; i < k; ++i)
+    {
+        res *= (n - i);
+        res /= (i + 1);
+    }
+
+    return res;
+}
+
+// A Binomial coefficient based function to find nth catalan number in O(n) time
+unsigned long int catalan(unsigned int n)
+{
+    // Calculate value of 2nCn
+    unsigned long int c = binomialCoeff(2 * n, n);
+
+    // return 2nCn/(n+1)
+    return c / (n + 1);
+}
+
+int main()
+{
+    for (int i = 0; i < 10; i++)
+        cout << catalan(i) << " ";
+    return 0;
+}
