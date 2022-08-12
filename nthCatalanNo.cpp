87eefd07b501390e5e64d2ea797e23b7ab441f92
Anushka Bhanot <83168678+AnushkaBhanot@users.noreply.github.com>
2022-08-12 12:12:44 +0530

    Update nthCatalanNo.cpp

--- nthCatalanNo.cpp
+++ nthCatalanNo.cpp
@@ -1,4 +1,17 @@
 // The first few Catalan numbers for n = 0, 1, 2, 3, … are 1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, …
+/* Same for: number of binary search trees,
+             number of parantheses,
+             number of ways a convex polygon of n+2 sides can split into triangles by connecting vertices,
+             number of paths with 2n steps on a rectangular grid from bottom left, i.e., (n-1, 0) to top right (0, n-1) that do not cross above the main diagonal,
+             number of ways to insert n pairs of parentheses in a word of n+1 letters,
+             number of noncrossing partitions of the set {1, …, 2n} in which every block is of size 2,
+             number of Dyck words of length 2n,
+             number of ways to tile a stairstep shape of height n with n rectangles,
+             number of ways to connect the points on a circle disjoint chords,
+             number of ways to form a “mountain ranges” with n upstrokes and n down-strokes that all stay above the original line,
+             number of stack-sortable permutations of {1, …, n},
+             number of permutations of {1, …, n} that avoid the pattern 123 (or any of the other patterns of length 3); that is, the number of permutations with no three-term increasing subsequence.
+*/
 
 #include<bits/stdc++.h>
 using namespace std;
